b0cf2f663d978c25074a50d89551787290f3fbde
Thom2920 <thomas.ross29@outlook.com>
2021-10-05 20:43:03 -0400

    Modifications de Startup.cs, de RepasController.cs et ajout de deux packages en vue de la lecture des données de la base de données

---
+++ Repas/Controllers/RepasController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace SiteSiteRepas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RepasController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public RepasController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        //[HttpGet]
+        //public JsonResult Get()
+        //{
+        //    string requete = @"
+        //                    select Id, Nom, Categorie from dbo.Repas";
+        //    DataTable table = new DataTable();
+        //    string sqlDataSource = _configuration.GetConnectionString();
+        //}
+    }
+}
