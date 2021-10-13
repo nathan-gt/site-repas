cde335f0a3a39e241805456e6815ed93379651f1
Thom2920 <thomas.ross29@outlook.com>
2021-10-12 21:47:14 -0400

    Reuqête POSTE fonctionnelle pour un repas et début du contrôleur famille

---
+++ Repas/Controllers/FamilleController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using SiteRepas.Models;
+
+namespace SiteSiteRepas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FamilleController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public FamilleController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        //Méthode pour l'obtention des données à l'aide de la méthode GET
+        [HttpGet]
+        public JsonResult Get()
+        {
+            DataTable table = new DataTable();
+
+            return new JsonResult(table);
+        }
+    }
+}
