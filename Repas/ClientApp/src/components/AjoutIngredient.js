9e379af92c328f10b78d8b6820cb7cc513d748d1
Quentin Salomon <quentin.salomon@outlook.fr>
2021-10-14 23:49:07 -0400

    Ajout d'un ingrédient : fonctionnel (non-conservé)

--- Repas/ClientApp/src/components/AjoutIngredient.js
+++ Repas/ClientApp/src/components/AjoutIngredient.js
@@ -5,24 +5,26 @@ export default function AjoutIngredient() {
     // Récupération du nom de l'ingrédient (input text)
     const refNomIngredient = useRef()
     // Création d'une liste d'ingrédients fictive
-    let ingredients = [
+    const [ingredients, setIngredients] = useState([
         "Sauce tomate",
         "Fromage",
         "Pepperoni",
         "Jambon",
         "Poivrons",
         "Champigons"
-    ]
+    ])
 
     // Fonction qui gère le click sur 
     // le bouton d'ajout d'un ingrédient.
     function gererAjoutIngr(e) {
+        // Récupération du contenu du TextBox
         const name = refNomIngredient.current.value
+        // Vérification si le contenu du 
+        // TextBox n'est pas vide
         if (name === '') return
-        console.log(name)
-        setIngredients(ingredients => {
-            return [...ingredients, [name]]
-        })
+        // Ajout de l'ingrédient à la liste
+        setIngredients(ingredientsPrec => [...ingredientsPrec, [name]])
+        // Vidage du TextBox
         refNomIngredient.current.value = null
     }
 
