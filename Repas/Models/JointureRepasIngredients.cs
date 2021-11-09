dc2ab8172d3a386d89f7c0907a4d451a987361be
Thom2920 <thomas.ross29@outlook.com>
2021-11-08 21:35:30 -0500

    Ajout d'un nouveau modèle de données avec deux clés étrangères

---
+++ Repas/Models/JointureRepasIngredients.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SiteSiteRepas.Models
+{
+    public class JointureRepasIngredients
+    {
+
+        [ForeignKey("UnRepas")]
+        public int RepasId { get; set; }
+        [ForeignKey("Ingredient")]
+        public int IngredientId { get; set; }
+    }
+}
